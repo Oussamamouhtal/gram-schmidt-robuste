94c191fb1eb8566e910dcd85c9bcec2950f5b1e9
Oussamamouhtal <oussamamouhtal22@gmail.com>
2025-06-01 15:41:11 -0400

    Ajout du fichier test.jl

--- test.jl
+++ test.jl
@@ -3,4 +3,18 @@ using Test
 include("gram-schmidt.jl")
 include("gram-schmidt-robuste.jl")
 
-@test 1 == 2  # FIXME
+
+us = [ [-1.0, 1, -1, 1], [-1.0, 3, -1, 3], [1.0, 3, 5, 7] ]
+us1 = [ [-1.0, 1, -1, 1], [-1.0, 3, -1, 3], [-1.0, 3, -1, 3] ]
+
+vs  = gramschmidtrobust(us)
+vs1 = gramschmidtrobust(us1)
+
+@test length(vs) == 3    # test sur le nombre de vecteur indépendant
+@test length(vs1) == 2   # test sur le nombre de vecteur indépendant
+
+@test norm(vs1[1]) == 1  # test sur la norme des vecteurs
+@test norm(vs1[2]) == 1  # test sur la norme des vecteurs
+
+@test dot(vs1[1], vs1[2])  == 0  # test sur l'orthogonalité
+
